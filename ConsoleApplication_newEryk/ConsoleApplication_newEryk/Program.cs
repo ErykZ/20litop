49e3ea604dff2b60e738013aaf3aa5cf1ed25f6a
ja <ja@JA-KOMPUTER>
2016-11-20 20:12:56 +0100

    dada

---
+++ ConsoleApplication_newEryk/ConsoleApplication_newEryk/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_newEryk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eryk");
+            Console.Read();
+        }
+    }
+}
